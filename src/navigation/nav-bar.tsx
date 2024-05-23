b24f74d73191571ba0690ba29c5d0644f16d23f5
Nana Kwame <iabu007@st.ug.edu.gh>
2024-05-23 18:22:53 +0000

    🚀update on commit

--- src/navigation/nav-bar.tsx
+++ src/navigation/nav-bar.tsx
@@ -103,42 +103,57 @@ export default function Navbar() {
 
           <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 pb-3 pt-2">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as="a"
-                href={HOME}
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
+              
+              <Link
+                to={HOME}
+                className={`flex items-center ${
+                  pathname === "/"
+                    ? "bg-gray-900 rounded-md text-[#0D7711] "
+                    : "border-transparent"
+                } mobile:px-4 mobile:pb-2  px-1 pt-1 text-xl font-medium text-primary`}
               >
                 Home
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href={ABOUT}
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              </Link>
+              <Link
+                to={ABOUT}
+                className={`flex items-center ${
+                  pathname === "/about"
+                    ? "bg-gray-900 rounded-md text-[#0D7711] "
+                    : "border-transparent"
+                } mobile:px-4 mobile:pb-2  px-1 pt-1 text-xl font-medium text-primary`}
               >
                 About Us
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              </Link>
+              <Link
+                to="#"
+                className={`flex items-center ${
+                  pathname === "/catalog"
+                    ? "bg-gray-900 rounded-md text-[#0D7711] "
+                    : "border-transparent"
+                } mobile:px-4 mobile:pb-2  px-1 pt-1 text-xl font-medium text-primary`}
               >
                 Catalog
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              </Link>
+              <Link
+                to="#"
+                className={`flex items-center ${
+                  pathname === "/learn"
+                    ? "bg-gray-900 rounded-md text-[#0D7711] "
+                    : "border-transparent"
+                } mobile:px-4 mobile:pb-2  px-1 pt-1 text-xl font-medium text-primary`}
               >
                 Learn
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href={CONTACT}
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              </Link>
+              <Link
+                to={CONTACT}
+                className={`flex items-center ${
+                  pathname === "/contact"
+                    ? "bg-gray-900 rounded-md text-[#0D7711] "
+                    : "border-transparent"
+                } mobile:px-4 mobile:pb-2  px-1 pt-1 text-xl font-medium text-primary`}
               >
                 Contact
-              </Disclosure.Button>
+              </Link>
             </div>
           </Disclosure.Panel>
         </>
